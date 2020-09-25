49e3051202f31311dd43e8d5a92e8c5f91d63d91
Ashwinkumar kadel <56585682+rutikkamble@users.noreply.github.com>
2020-09-25 21:29:37 +0530

    Add files via upload

---
+++ Atomation.py
@@ -0,0 +1,5 @@
+import requests
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path= r'C:\Users\cu2uy\AppData\Local\Programs\Python\Python38\Drivers\chromedriver.exe')
+driver.get('http://youtube.com')
